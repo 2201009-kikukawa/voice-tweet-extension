f96cc690e557ddee71c710db3fadc997faedc62a
CRH-ayyamada <141080366+yamada1023@users.noreply.github.com>
2025-05-27 12:44:33 +0900

    update: サイドパネル追加

--- src/extension.ts
+++ src/extension.ts
@@ -1,10 +1,13 @@
 import { ExtensionContext, window } from "vscode";
 import { ViewProvider } from "./providers/ViewProvider";
+import { SidebarProvider } from "./providers/SidebarProvider";
 
 export function activate(context: ExtensionContext) {
-  const provider = new ViewProvider(context.extensionUri);
+  const viewProvider = new ViewProvider(context.extensionUri);
+  const sidebarProvider = new SidebarProvider(context.extensionUri);
 
-  const sampleViewDisposable = window.registerWebviewViewProvider(ViewProvider.viewType, provider);
+  const sampleViewDisposable = window.registerWebviewViewProvider(ViewProvider.viewType, viewProvider);
+  const sidebarViewDisposable = window.registerWebviewViewProvider(SidebarProvider.viewType, sidebarProvider);
 
-  context.subscriptions.push(sampleViewDisposable);
+  context.subscriptions.push(sampleViewDisposable, sidebarViewDisposable);
 }
