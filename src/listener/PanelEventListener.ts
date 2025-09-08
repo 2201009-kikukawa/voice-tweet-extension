6a23a3b00bca89e444ee1a515daef333b1224f61
yamada <141080366+yamada1023@users.noreply.github.com>
2025-09-08 10:10:12 +0900

    パネルから設定タブを開けるようにする

--- src/listener/PanelEventListener.ts
+++ src/listener/PanelEventListener.ts
@@ -1,10 +1,14 @@
-import { WebviewView } from "vscode";
+import { Uri, WebviewView } from "vscode";
 import { EventListenerProps, EventTypes } from "../types/classNames";
-import { SidebarEventListener } from "./SidebarEventListener";
+import { TabEventListener } from "./TabEventListener";
+import { TabProvider } from "../providers/TabProvider";
 
 export let panelWebviewView: WebviewView | undefined;
+let context: Uri;
 
 export class PanelEventListener {
+  constructor(extensionUrl: Uri) { context = extensionUrl; }
+
   public setWebviewMessageListener(webviewView: WebviewView) {
     panelWebviewView = webviewView;
     webviewView.webview.onDidReceiveMessage((message: EventListenerProps) => {
@@ -14,9 +18,13 @@ export class PanelEventListener {
         case EventTypes.initTimer:
           webviewView.webview.postMessage({
             type: EventTypes.receiveMessage,
-            text: SidebarEventListener.lastMessage || "",
+            text: TabEventListener.lastMessage || "",
             speakerId: 0
           });
+          break;
+        case EventTypes.openSettingTab:
+          const tab = new TabProvider(context);
+          tab.setTabView();
           break;
         default:
           console.warn(`Unknown message type: ${type}`);
